1eedf03714a79cc6050016437f741a0fce5a04f6
Katarzyna Wójcik <katarzynawojcik76@gmail.com>
2025-06-16 20:24:48 +0200

    Done

--- perceptron.py
+++ perceptron.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+import os
+
+path = 'C:/Users/katar/PycharmProjects/PythonProject2/perceptron'
 
 class Perceptron:
     def __init__(self, x1, x2, y_d, bias, alpha, w1, w2, end):
@@ -10,16 +13,21 @@ class Perceptron:
         self.end = end
         self.w1 = w1
         self.w2 = w2
+
     def train(self):
         epoch_num = 1
         def epoch():
             nonlocal epoch_num
             err = []
-            print(f'Epoka {epoch_num}')
-            print(
-                f'{"x1":^3} | {"x2":^3} | {"w1":^5} | {"w2":^5} | {"Yd":^3} | {"Yp":^3} | {"e":^3} | {"dw1":^5} | {"dw2":^5}')
+            line1 = f'Epoka {epoch_num}\n'
+            line2 = f'{"x1":^3} | {"x2":^3} | {"w1":^5} | {"w2":^5} | {"Yd":^3} | {"Yp":^3} | {"e":^3} | {"dw1":^6} | {"dw2":^6}\n'
+            print(line1)
+            print(line2)
+            with open('training.txt', 'a') as f:
+                f.write(line1)
+                f.write(line2)
             for i in range(len(self.y_d)):
-                w_sum = self.x1[i]*self.w1 + self.x2[i]*self.w2
+                w_sum = self.x1[i]*self.w1 + self.x2[i]*self.w2 - self.bias
                 if w_sum >= self.bias:
                     y_p = 1
                 else:
@@ -28,11 +36,15 @@ class Perceptron:
                 err.append(e)
                 d_w1 = self.alpha * self.x1[i] * e
                 d_w2 = self.alpha * self.x2[i] * e
-                print(
-                    f'{self.x1[i]:^3} | {self.x2[i]:^3} | {self.w1:^5.2f} | {self.w2:^5.2f} | {self.y_d[i]:^3} | {y_p:^3} | {e:^3} | {d_w1:^5.2f} | {d_w2:^5.2f}')
+                lines = f'{self.x1[i]:^3} | {self.x2[i]:^3} | {self.w1:^5.2f} | {self.w2:^5.2f} | {self.y_d[i]:^3} | {y_p:^3} | {e:^3} | {d_w1:^6.2f} | {d_w2:^6.2f}\n'
+                print(lines)
+                with open('training.txt', 'a') as f:
+                    f.write(lines)
                 self.w1 += d_w1
                 self.w2 += d_w2
             print('\n')
+            with open('training.txt', 'a') as f:
+                f.write('\n')
             return err
 
         errors_tab = []
@@ -51,20 +63,27 @@ class Perceptron:
         #Błąd na epokę
         plt.figure()
         plt.plot([i+1 for i in range(epochs-1)], errors)
+        plt.xticks([i+1 for i in range(epochs-1)])
         plt.xlabel('Epoka')
         plt.ylabel('Błąd')
         plt.title('Średni błąd na epokę')
-        plt.show()
+        plt.savefig('error.jpg')
 
-x1 = (1, 9.4, 2.5, 8)
-x2 = (1, 6.4, 2.1, 7.7)
-y_d = (1, -1, 1, -1)
-bias = -3
-alpha = 0.5
-w1 = 1
-w2 = 1
-end = 10
+        #Separowalność - hardcoded bo wyliczanie ze wzoru który znalazłam nie działało :(
+        #Możliwe że perceptron sobie to inaczej oddziela ale przynajmniej widać że problem separowalny?
+        sep_x = (2.5 + 8) / 2
+        sep_y = (2.1 + 6.4) / 2
+        plt.figure()
+        plt.scatter(self.x1, self.x2)
+        plt.axline((sep_x, sep_y), slope = -1)
+        plt.xlabel('x1')
+        plt.ylabel('x2')
+        plt.title('Separowalność')
+        plt.savefig('sep.jpg')
 
-p = Perceptron(x1, x2, y_d, bias, alpha, w1, w2, end)
-e, epochs = p.train()
-p.plots(e, epochs)
+os.chdir(path)
+
+#Initializing and running
+p = Perceptron(x1 = (1, 9.4, 2.5, 8), x2 = (1, 6.4, 2.1, 7.7), y_d = (1, -1, 1, -1), bias = -1.5, alpha = 0.55, w1 = 1, w2 = 1, end = 10)
+error_table, number_of_epochs = p.train()
+p.plots(error_table, number_of_epochs)
